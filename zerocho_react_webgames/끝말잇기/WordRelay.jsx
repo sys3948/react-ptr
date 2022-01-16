24575337417bd9c5abe3b73c931af5f9229c6f8d
yun <sys394880@gmail.com>
2022-01-16 17:36:07 +0900

    commit 12 : zero cho react web game - 끗말잇기를 훅으로 변경 적용하기.

--- zerocho_react_webgames/끝말잇기/WordRelay.jsx
+++ zerocho_react_webgames/끝말잇기/WordRelay.jsx
@@ -1,5 +1,6 @@
 const React = require('react'); // npm에서 react를 불러와야지 하나의 html 파일에서 컴포넌트를 생성 하는 것이 아닌 여러개의 jsx or js 파일로 컴포넌트를 생성할 수 있다.
 const {Component} = React;
+const {useState, useRef} = React;
 
 class WordRelay extends Component{
   state = {
@@ -52,4 +53,43 @@ class WordRelay extends Component{
   };
 }
 
-module.exports = WordRelay; // node의 module system으로 외부에서 WordRelay 컴포넌트를 load하기 위해서 작성해줘야한다.
+
+
+const WordRelayHooks = () => {
+    const [word, setWord] = useState('윤');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef(null);
+
+
+    const onChange = (e) => {
+      setValue(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      if(word[word.length - 1] === value[0]){
+        setWord(value);
+        setResult('정답!');
+        setValue('');
+        inputRef.current.focus();
+      } else {
+        setResult('땡!');
+        setValue('');
+        inputRef.current.focus();
+      }
+    }
+
+    return (
+      <>
+        <div>{word}</div>
+        <form onSubmit={onSubmit}>
+          <input ref={inputRef} value={value} onChange={onChange} />
+          <input type="submit" value="입력" / >
+        </form>
+        <div>{result}</div>
+      </>
+    );
+}
+
+module.exports = WordRelayHooks; // node의 module system으로 외부에서 WordRelay 컴포넌트를 load하기 위해서 작성해줘야한다.
