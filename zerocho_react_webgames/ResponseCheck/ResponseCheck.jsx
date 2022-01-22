edb175c41ba95ccb627ef63d3c5487f955186480
yun <sys394880@gmail.com>
2022-01-22 17:59:44 +0900

    commit 19 : zero cho react web game - reset 버튼 구현, 반응속도 체크 Hooks로 구현.

--- zerocho_react_webgames/ResponseCheck/ResponseCheck.jsx
+++ zerocho_react_webgames/ResponseCheck/ResponseCheck.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useRef } from 'react';
 
 
 class ResponseCheck extends Component {
@@ -43,10 +43,21 @@ class ResponseCheck extends Component {
         }
     }
 
+
+    onReset = () => {
+        this.setState({
+            result : [],
+        });
+    }
+
     resultScreen = () => {
         return (
             this.state.result.length === 0 ? 
-            null : <div>평균 시간 : {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+            null : 
+            <>
+                <div>평균 시간 : {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+                <button onClick={this.onReset}>리셋</button>
+            </>
         );
     }
 
@@ -63,4 +74,68 @@ class ResponseCheck extends Component {
     }
 }
 
-export default ResponseCheck;
+
+const ResponseCheckHooks = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요.');
+    const [result, setResult] = useState([]);
+
+    // Class Component일 때의 Ref는 DOM의 접근할 때 사용한 것이다.
+    // Hooks에서의 Ref는 Class의 this의 기능도 가지고 있다.
+    // 또한 Hooks의 Ref(useRef)는 Hooks에서 State를 변경하면 Hooks 전체가 다시 렌더링이 되는데
+    // Ref부분만 렌더링이 되지 않고 변하는 값만 기록해둔다.
+    // Ref를 사용할 경우 current로 접근해야한다는 것을 잊지 말아야한다.
+    const timeout = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
+
+    const onClickScreen = () => {
+        if(state === 'waiting'){
+            setState('ready');
+            setMessage('초록색이 되면 글릭하세요.');
+            timeout.current = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();
+            }, Math.floor(Math.random() * 1000) + 2000);
+        } else if(state === 'ready'){
+            clearTimeout(timeout.current); // 위 setTimeout() 초기화 해주기.
+            setState('waiting');
+            setMessage('너무 성급하셨군요. 초록색이 된 후에 클릭하세요.');
+        } else if(state === 'now'){
+            endTime.current = new Date();
+            setState('waiting');
+            setResult((prevResult) => {
+                return([...prevResult, endTime.current - startTime.current]);
+            });
+            setMessage('클릭해서 시작하세요.');
+        }
+    }
+
+    const onReset = () => {
+        setResult([]);
+    }
+
+    const resultScreen = () => {
+        return (
+            result.length === 0 ? 
+            null : 
+            <>
+                <div>평균 시간 : {result.reduce((a, c) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>리셋</button>
+            </>
+        );
+    }
+
+    return(
+        <>
+            <div id='screen' className={state} onClick={onClickScreen}>
+                {message}
+            </div>
+            {/* React에서의 조건문은 아래와 같이 삼항 연산자 또는 부호 연산자를 사용한다. */}
+            {resultScreen()}
+        </>
+    );
+}
+
+export default ResponseCheckHooks;
