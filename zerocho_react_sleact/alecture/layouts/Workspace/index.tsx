37a251cc0c966b2efbc9cabd670d13ac701738e6
yun <sys394880@gmail.com>
2022-03-20 23:43:52 +0900

    commit 49 : zero cho sleact - 메뉴 만들기

--- zerocho_react_sleact/alecture/layouts/Workspace/index.tsx
+++ zerocho_react_sleact/alecture/layouts/Workspace/index.tsx
@@ -1,20 +1,22 @@
-import { Header, RightMenu, ProfileImg, WorkspaceWrapper, Workspaces, Channels, Chats, WorkspaceName, MenuScroll } from '@layouts/Workspace/styles';
+import { Header, RightMenu, ProfileImg, WorkspaceWrapper, Workspaces, Channels, Chats, WorkspaceName, MenuScroll, ProfileModal, LogOutButton } from '@layouts/Workspace/styles';
 
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 
-import React, {FC, useCallback} from "react";
+import React, {FC, useCallback, useState} from "react";
 import useSWR from 'swr';
 import {Routes, Route, Navigate, useParams} from 'react-router-dom';
 import gravatar from 'gravatar';
 
 import loadable from '@loadable/component';
+import Menu from '@components/Menu';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Workspace:FC = ({children}) => {
   const {data, error, mutate} = useSWR('http://localhost:3095/api/users', fetcher);
+  const [showUserMenu, setShowUserMenu] = useState(false);
 
   const onLogout = useCallback(() => {
     axios.post('http://localhost:3095/api/users/logout', null, {
@@ -22,6 +24,11 @@ const Workspace:FC = ({children}) => {
     }).then((response) => {
         mutate(false, false);
     });
+  }, []);
+
+  const onClickUserProfile = useCallback(() => {
+    // 토글 함수.
+    setShowUserMenu((prev) => !prev);
   }, []);
 
   if(!data){
@@ -32,18 +39,28 @@ const Workspace:FC = ({children}) => {
     )
   }
 
-  console.log('Workspace 입니다.')
+  console.log('Workspace 입니다.');
+  console.log(data);
 
   return(
     <div>
       <Header>
         <RightMenu>
-          <span>
-            <ProfileImg src={gravatar.url(data.email, {s : '28px', d : 'retro'})} alt={data.ninckname}></ProfileImg>
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(data.email, {s : '28px', d : 'retro'})} alt={data.nickname}></ProfileImg>
+            { showUserMenu && (<Menu style={{ right:0, top:38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+              <ProfileModal>
+                <img src={gravatar.url(data.email, {s : '36px', d : 'retro'})} alt={data.nickname} />
+                <div>
+                  <span id='profile-name'>{data.nickname}</span>
+                  <span id='profile-active'>Active</span>
+                </div>
+              </ProfileModal>
+              <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+            </Menu>)}
           </span>
         </RightMenu>
       </Header>
-      <button onClick={onLogout}>로그 아웃</button>
       <WorkspaceWrapper>
         <Workspaces></Workspaces>
         <Channels>
