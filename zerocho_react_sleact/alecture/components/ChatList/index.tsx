c4c3d7ff7657cb10383fb482724cca8731afb721
yun <sys394880@gmail.com>
2022-04-26 22:49:55 +0900

    commit 62 : zero cho sleact -  커스텀 스크롤바와 dayjs

--- zerocho_react_sleact/alecture/components/ChatList/index.tsx
+++ zerocho_react_sleact/alecture/components/ChatList/index.tsx
@@ -1,19 +1,24 @@
-import React, { VFC } from "react";
+import React, { useCallback, useRef, VFC } from "react";
 
 import {ChatZone, Section} from "@components/ChatList/styles";
 import { IDM } from "@typings/db";
 import Chat from "@components/Chat";
+import { Scrollbars } from "react-custom-scrollbars";
 
 interface Props {
   chatData? : IDM[];
 }
 
 const ChatList : VFC<Props> = ({chatData}) => {
+  const scrollRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
   return (
     <ChatZone>
-      {chatData?.map((chat) => {
-        return <Chat key={chat.id} data={chat} />
-      })}
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => {
+          return <Chat key={chat.id} data={chat} />
+        })}
+      </Scrollbars>
     </ChatZone>
   )
 }
